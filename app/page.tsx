ab9eb46ec76b7c4bf5e60820031a90afee0b3c4e
kennde3 <nniyonshutikenny123@gmail.com>
2026-03-06 16:33:43 +0200

    003

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <div>
-        <div>Home</div>
-        <div>Product</div>
-        <div>About Us</div>
-        <div>Contact Us</div>
+      <div className="flex justify-around bg-yellow-500 -mx-60 p-4">
+        <div className="bg-green-500 ">Lumera Logo</div>
+        <div className="flex bg-[red]  space-x-4 p-1.5">
+          <Link href={""}>Home</Link>
+          <Link href={""}>Product</Link>
+          <Link href={""}>About Us</Link>
+          <Link href={""}>Contact Us</Link>
+        </div>
       </div>
     </>
   );
